53b73c9e08510b81d0d3e14b4675bfd1f279791e
KilianGitHub97 <66937025+KilianGitHub97@users.noreply.github.com>
2021-07-08 18:22:35 +0200

    Hyperparameter Tuning

--- code/main.py
+++ code/main.py
@@ -10,7 +10,16 @@ import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
-
+#Model selection
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report
+#Models
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import svm
 #----- setup -----#
 
 #set working directory
@@ -129,3 +138,95 @@ desc_stat = {
 #convert dictionary to dataframe
 desc_stat = pd.DataFrame(desc_stat)
 
+#delete unneeded variables
+del [i,
+     park_no,
+     park_yes]
+#----- Predict early-onset parkinson disease -----#
+
+#prepare data
+X_pred = X_nona.loc[:, X_nona.columns != "participant_code"]
+
+#split data into train/test by a 2/10 vs 8/10 ratio for model assessment
+x_train, x_test, y_train, y_test = train_test_split(X_pred.drop("parkinson", axis=1),
+                                                    X_pred["parkinson"],
+                                                    test_size = 0.2)
+
+#is the assessment data still balanced?
+len(y_train)
+len(y_test)
+y_train.value_counts(normalize=True)
+y_test.value_counts(normalize=True)
+
+#scale dataset
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train, y_train)
+x_test = sc.transform(x_test)
+
+#10-Fold Cross Validation
+rkf_validation = RepeatedKFold(n_splits=10, n_repeats=5)
+
+#Randomized Grid
+#LDA has a closed-form solution and therefore has no hyperparameters.
+#The solution can be obtained using the empirical sample class covariance
+#matrix. Shrinkage is used when there are not enough samples. In that case
+#the empirical covariance matrix is often not a very good estimator.
+models_hyperparameters = {
+    "logistic_regression" : {
+        "model" : LogisticRegression(solver="liblinear"),
+        "hyperparameters" : {
+            "penalty" : ["l1", "l2"],
+            "C" : [0.5, 0.7, 1, 3, 5, 10]
+            }
+        },
+    "linear_discriminant_analysis" : {
+        "model" : LinearDiscriminantAnalysis(solver="lsqr"),
+        "hyperparameters" : {
+            "shrinkage": list(np.arange(0,1,0.1))
+            }
+        },
+    "support_vector_machine" : {
+        "model" : svm.SVC(gamma = "auto", shrinking = True),
+        "hyperparameters" : {
+            "kernel" : ["linear", "poly", "rbf", "sigmoid"],
+            "C" : [0.1, 0.5, 1, 1.5, 2, 5, 10],
+            "coef0" : [0.1, 0.5, 0.8, 1, 1.5, 2, 5],
+            "degree" : [i for i in range(1,6)]
+            }
+        }
+}
+
+#results from hyperparameter tuning
+results = []
+
+#hyperparameter tuning
+for model_name, mp in models_hyperparameters.items():
+    model = GridSearchCV(mp["model"], 
+                         mp["hyperparameters"],
+                         cv=rkf_validation,
+                         return_train_score=False)
+    model.fit(x_train, y_train)
+    results.append({
+        "model": model_name,
+        "best_score": model.best_score_,
+        "best_params": model.best_params_
+    })
+
+results = pd.DataFrame(results, columns=['model','best_score','best_params'])
+
+#a priori probability
+X["parkinson"].value_counts(normalize=True)
+
+#get best parameters
+par = results.iat[2,2]
+
+#recreate model
+supvec = svm.SVC(gamma = "auto",
+                 shrinking = True,
+                 C = par.get("C"),
+                 coef0 = par.get("coef0"),
+                 degree = par.get("degree"),
+                 kernel = par.get("kernel"))
+
+supvec.fit(x_train, y_train)
+supvec.score(x_test, y_test)
